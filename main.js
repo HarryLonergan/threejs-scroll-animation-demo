504a7a410f78562f1c8cfe66e6c0ca2b1c5f1b0c
Harry Lonergan <harryllonergan@gmail.com>
2023-09-07 14:00:45 +1000

    PPH website test

--- main.js
+++ main.js
@@ -65,11 +65,12 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+const PPHTexture = new THREE.TextureLoader().load('PPH.jpg');
 
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+const PPH = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: PPHTexture }));
 
-scene.add(jeff);
+scene.add(PPH);
+
 
 // Moon
 
@@ -89,8 +90,8 @@ scene.add(moon);
 moon.position.z = 30;
 moon.position.setX(-10);
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+PPH.position.z = -5;
+PPH.position.x = 2;
 
 // Scroll Animation
 
@@ -100,8 +101,8 @@ function moveCamera() {
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+  PPH.rotation.y += 0.01;
+  PPH.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
